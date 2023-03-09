7aa9c49015872b11af7fd721e97bc56d284c3d30
Yirou Qiu <y2qiu@uwaterloo.ca>
2023-03-09 09:34:31 -0500

    add search view. version0.1

--- core/urls.py
+++ core/urls.py
@@ -7,6 +7,7 @@ from core.api.password import ChangePasswordView
 from core.api.profile import ProfileViewSet
 from core.api.coupon import CouponViewSet
 from core.api.bill import *
+from core.api.billSearch import SearchBillListView
 
 router = routers.DefaultRouter()
 
@@ -32,5 +33,8 @@ urlpatterns += [
 
     # bill
     path('api/bill/', bill_list_create_api, name="bill-list"),
-    path('api/bill/<uuid:pk>/', bill_detail_api, name="bill-detail")
+    path('api/bill/<uuid:pk>/', bill_detail_api, name="bill-detail"),
+
+    # bill search
+    path('api/bill/search/', SearchBillListView.as_view(), name="bill-search")
 ]
